dc6e2834604ef09c4793aaa83e00e60c420f9c61
Reuben D'Souza <reubenjds@gmail.com>
2025-06-06 18:29:04 -0400

    feat: add designed by

--- app/page.tsx
+++ app/page.tsx
@@ -227,7 +227,7 @@ export default function Home() {
 
 			{/* Footer - Simplified */}
 			<footer className='bg-gray-800 text-white py-6'>
-				<div className='container mx-auto px-4'>
+				<div className='flex flex-col mx-auto px-4 justify-between items-center gap-2'>
 					<div className='flex flex-col md:flex-row justify-center items-center'>
 						<div className='flex items-center mb-4 md:mb-0'>
 							<span className='font-bold text-lg mr-2'>
@@ -240,6 +240,9 @@ export default function Home() {
 							<span className='text-gray-300 text-sm'>•</span>
 							<span className='text-gray-300 text-sm mx-2'>(613) 123-4567</span>
 						</div>
+					</div>
+					<div className='text-gray-300 text-sm mx-2'>
+						Designed by Eric & Team
 					</div>
 				</div>
 			</footer>
